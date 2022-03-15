c427c179753bf0c40d553177880c66abe54af451
nza6nq <nza6nq@virginia.edu>
2022-03-14 22:51:11 -0400

    Model Loading Methods Change and Alteration of Antibiotic of Interest
    -Changed to load model from Sco.xml instead of Sco.mat, as the .mat file has issues and the .xml file provides the correct model
    -Streptomycin production is not a factor in the model - there are no genes essential to the streptomycin production. The only reaction involving streptomycin production is the consumption of streptomycin to produce streptomycin 6-phosphate.
    -Actinorhodin was selected as the antibiotic of interest because it is connected to a number of reactions in the model, including synthesis and consumption. Furthermore, KEGG indicates many of the reactions associated with its production are fairly unique to this bacteria.

--- streptomycin_project.m
+++ streptomycin_project.m
@@ -5,11 +5,27 @@ clear all; close all;
 addpath("../cobratoolbox")
 initCobraToolbox;
 
+clc
 %% Load data
-load('Sco.mat')
+model = readCbModel('Sco.mat');
+
+%% Alter optimization parameter for study of antibiotic production
+%check current optimization parameter
+printObjective(model);
+%gather information about the reactions involved in actinorhodin
+%surfNet(model, 'actinorhodin')
+%surfNet(model, 'ACTS19')
+%change optimization parameter to actinorhodin production reaction
+model = changeObjective(model, 'ACTS19');
 
 %% Calculate Essential Genes
-[grRatio, grRateKO, grRateWT, hasEffect, delRxns, fluxSolution]...
+[gene_grRatio, gene_grRateKO, gene_grRateWT, gene_hasEffect, gene_delRxns, gene_fluxSolution]...
     = singleGeneDeletion(model);
-essential_genes_ind = find(grRateKO == 0);
+essential_genes_ind = find(gene_grRatio <= 0.01);
 essential_genes = model.genes(essential_genes_ind);
+
+%% Calculate Essential Reactions
+[rxn_grRatio, rxn_grRateKO, rxn_grRateWT, rxn_hasEffect, rxn_delRxn, rxn_fluxSolution]...
+    = singleRxnDeletion(model);
+essential_rxn_ind = find(rxn_grRatio <= 0.01);
+essential_rxns = model.rxnNames(essential_rxn_ind);
